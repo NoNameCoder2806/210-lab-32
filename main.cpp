46f08e94982d877579cecd0f59ca1739f605ab4a
NoNameCoder2806 <hoangvien2806@gmail.com>
2025-11-09 22:08:27 -0800

    Setting up Lab 32

---
+++ main.cpp
@@ -0,0 +1,27 @@
+// COMSC-210 | Lab 32 | Dat Hoang Vien
+// IDE used: Visual Studio Code
+
+// Libraries
+#include <iostream>
+using namespace std;
+
+// Constants
+
+// Function prototypes
+
+// Main function
+int main()
+{
+    return 0;
+}
+
+// Function implementations
+/*
+    function_name()
+    What the function does
+    Arguments: 
+        - argument 1: what it does
+        - argument 2: what it does
+    Return:
+        - what the function returns
+*/
