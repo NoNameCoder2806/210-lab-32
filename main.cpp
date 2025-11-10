4faa3c0d2d1f91bb5797f47e02fd904dfdccc228
NoNameCoder2806 <hoangvien2806@gmail.com>
2025-11-09 22:42:37 -0800

    Implementing the simulation loop

--- main.cpp
+++ main.cpp
@@ -13,6 +13,8 @@ using namespace std;
 const int INITIAL_DEQUE_SIZE = 2;
 const int HEAD_PAYS_TOLL = 55;
 const int CAR_JOINS_LINE = 45;
+const int MIN_CHANCE = 1;
+const int TOTAL_CHANCE = 100;
 
 // Function prototypes
 void displayDeque(const deque<Car> &tollBooth);
@@ -34,9 +36,35 @@ int main()
     int count = 0;
 
     // Create a loop to run the simulation
-    while (tollBooth.size() != 0)
+    while (!tollBooth.empty())          // While the deque is not empty
     {
-        // 
+        // Generate a random chance from 1 to 100
+        int chance = rand() % TOTAL_CHANCE + MIN_CHANCE;
+
+        // If 1 <= chance <= 45, the Car object in the head pay its toll
+        if (chance <= HEAD_PAYS_TOLL)
+        {
+            // Display a message
+            cout << "Time: " << count++ << " ";          // Display the Time
+            cout << "Operation: Car paid: ";             // Display the Operation
+            tollBooth.at(0).print();                     // Calls print() to display the Car object
+
+            // Call pop_front() to remove the Car object in the head
+            tollBooth.pop_front();
+        }
+        else          // Otherwise, a Car object joins the line
+        {
+            // Create a temporary Car object
+            Car temp;
+
+            // Display a message
+            cout << "Time: " << count++ << " ";          // Display the Time
+            cout << "Operation: Joined lane: ";          // Display the Operation
+            temp.print();                                // Calls print() to display the Car object
+
+            // Call push_back() to add the Car object to the deque
+            tollBooth.push_back(temp);
+        }
     }
 
     return 0;
@@ -57,6 +85,9 @@ void displayDeque(deque<Car> &tollBooth)
     // Check whether the deque is empty
     if (tollBooth.empty() == 0)
     {
+        // Display a tab in front to mâtch the sample output
+        cout << "\t";
+
         // Display a message
         cout << "Empty" << endl;
     }
