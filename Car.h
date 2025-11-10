ac3fc9a97b318733ace7b1f9a88837a12d4bf0da
NoNameCoder2806 <hoangvien2806@gmail.com>
2025-11-09 22:13:59 -0800

    Added comments for the Car class

--- Car.h
+++ Car.h
@@ -1,39 +1,87 @@
+// COMSC-210 | Lab 32 | Dat Hoang Vien
+// IDE used: Visual Studio Code
+
+// Define the Car class
 #ifndef CAR_H
 #define CAR_H
+
+// Libraries
 #include <iostream>
 #include <iomanip>
 using namespace std;
 
+// Constants
 const int YEAR_MIN = 1990, YEAR_MAX = 2024;
 const int XP_MIN = 1000, XP_MAX = 9999;
 const int NR_MANUFACTURERS = 25;
 
-class Car {
+// Car class
+class Car
+{
 private:
-    string make;
-    int year;
-    int transponder;
+    string make;              // The make of the Car object
+    int year;                 // The manufactured year of the Car object
+    int transponder;          // The transponder number of the Car object
+
+    // All the possible manufacturers
     string manufacturers[NR_MANUFACTURERS] = 
         {"Toyota", "Ford", "Volkswagen", "Honda", "Chevrolet", "Nissan", "BMW", 
          "Mercedes-Benz", "Audi", "Hyundai", "Kia", "Subaru", "Mazda", "Tesla", 
          "Jaguar", "Fiat", "Land Rover", "Mitsubishi", "Peugeot", "Renault", 
          "Porsche", "Lexus", "Volvo", "Skoda", "Infiniti"};
+
 public:
-    Car() {
+    // Default constructor
+    Car()
+    {
         make = manufacturers[rand() % NR_MANUFACTURERS];
         year = rand() % (YEAR_MAX - YEAR_MIN + 1) + YEAR_MIN;
         transponder = rand() % (XP_MAX - XP_MIN + 1) + XP_MIN;
     }
-    string getMake()          { return make; }
-    int getYear()             { return year; }
-    int getTransponder()      { return transponder; }
-    void setMake(string mk)   { make = mk; }
-    void setYear(int yr)      { year = yr; }
-    void setTransponder(int t) { transponder = t; }
 
-    void print() {
+    // Accessors / Getters
+    string getMake()          
+    {
+        return make;
+    }
+
+    int getYear()             
+    {
+        return year;
+    }
+
+    int getTransponder()     
+    { 
+        return transponder;
+    }
+
+    // Mutators / Setters
+    void setMake(string mk)   
+    {
+        make = mk;
+    }
+
+    void setYear(int yr)      
+    {
+        year = yr;
+    }
+
+    void setTransponder(int t) 
+    { 
+        transponder = t;
+    }
+
+    // Member function
+    /*
+        print()
+        Print the information of the Car object
+    */
+    void print()
+    {
         cout << "[" << year << " " << make << " (" << transponder << ")]\n";
     }
 
 };
+
+// End of the Car.h file
 #endif
