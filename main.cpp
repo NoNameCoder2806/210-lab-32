748e2080e305159067968cabe835d8562df8fab6
NoNameCoder2806 <hoangvien2806@gmail.com>
2025-11-09 22:32:58 -0800

    Implementing a displayDeque() function

--- main.cpp
+++ main.cpp
@@ -4,25 +4,39 @@
 // Libraries and headers
 #include <iostream>
 #include <deque>
+#include <cstdlib>
+#include <ctime>
 #include "Car.h"
 using namespace std;
 
 // Constants
 const int INITIAL_DEQUE_SIZE = 2;
+const int HEAD_PAYS_TOLL = 55;
+const int CAR_JOINS_LINE = 45;
 
 // Function prototypes
+void displayDeque(const deque<Car> &tollBooth);
 
 // Main function
 int main()
 {
+    // Call srand() and time()
+    srand(time(0));
+
     // Create a deque containing Car objects
     deque<Car> tollBooth(INITIAL_DEQUE_SIZE);
 
-    // Display the initial deque by iterating through the deque
-    for (int i = 0; i < tollBooth.size(); i++)
+    // Display the initial deque
+    cout << "Initial queue: " << endl;
+    displayDeque(tollBooth);
+
+    // Create an int to count the number of simulations
+    int count = 0;
+
+    // Create a loop to run the simulation
+    while (tollBooth.size() != 0)
     {
-        // Display the Car object
-        tollBooth[i].print();
+        // 
     }
 
     return 0;
@@ -38,3 +52,22 @@ int main()
     Return:
         - what the function returns
 */
+void displayDeque(deque<Car> &tollBooth)
+{
+    // Check whether the deque is empty
+    if (tollBooth.empty() == 0)
+    {
+        // Display a message
+        cout << "Empty" << endl;
+    }
+
+    // Iterate through the deque
+    for (int i = 0; i < tollBooth.size(); i++)
+    {
+        // Display a tab in front to mâtch the sample output
+        cout << "\t";
+
+        // Use the print() member function to display the Car object
+        tollBooth.at(i).print();
+    }
+}
