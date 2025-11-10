c0cfcdca8710282ad1b2f39841f70cc7d87af58e
NoNameCoder2806 <hoangvien2806@gmail.com>
2025-11-09 22:22:42 -0800

    Milestone 1

--- main.cpp
+++ main.cpp
@@ -1,17 +1,30 @@
 // COMSC-210 | Lab 32 | Dat Hoang Vien
 // IDE used: Visual Studio Code
 
-// Libraries
+// Libraries and headers
 #include <iostream>
+#include <deque>
+#include "Car.h"
 using namespace std;
 
 // Constants
+const int INITIAL_DEQUE_SIZE = 2;
 
 // Function prototypes
 
 // Main function
 int main()
 {
+    // Create a deque containing Car objects
+    deque<Car> tollBooth(INITIAL_DEQUE_SIZE);
+
+    // Display the initial deque by iterating through the deque
+    for (int i = 0; i < tollBooth.size(); i++)
+    {
+        // Display the Car object
+        tollBooth[i].print();
+    }
+
     return 0;
 }
 
